45b26b94e24980421b6e625de5705f6c298d0c02
sicel <sicelubuhlen@gmail.com>
2020-08-06 11:48:29 +0200

    task 1-3

---
+++ js/myscript.js
@@ -0,0 +1,131 @@
+// TASK 1 Answer...........................................................................
+
+// x = 0
+// y = 1
+// Print the value of x
+// Print the value of y
+// x = x + 3
+// y = y + x
+// Print the value of x
+// Print the value of y
+
+
+// answer........................................................................................
+// part 1
+
+let x = 0;
+let y = 1;
+
+console.log(x);
+console.log(y);
+
+// Part 2
+
+let X = x + 3;
+let Y = y + X;
+
+console.log(X);
+console.log(Y);
+
+
+// TASK 2 Question........................................................................
+
+// x = 1 + 1 * 2
+// y = (1 + 1) * 2
+// z = 1 + ( 1 * 2 )
+// a =  1 + 1 * 2 / 2
+// b =  (1 + 1 * 2 ) /  2
+
+// answer................................................................................
+
+let Resultx = 1 + 1 * 2;
+let Resulty = (1 + 1) * 2;
+let Resultz = 1 + ( 1 * 2 );
+let Resulta =  1 + 1 * 2 / 2;
+let Resultb =  (1 + 1 * 2 ) /  2;
+
+console.log(Resultx);
+console.log(Resulty);
+console.log(Resultz);
+console.log(Resulta);
+console.log(Resultb);
+
+// TASK 3 Question.........................................................................
+// Write a function that takes 2 numbers as input. If either of the numbers is 65, OR if the sum of the numbers is 65 then return true. Otherwise return false
+
+// Answer...............................................................................
+
+const always = 65;
+let numOne = 20;
+let numTwo = 30;
+
+let sum = numOne + numTwo ;
+
+if (sum = always){
+    console.log("true");
+}else if (sum != always){
+    console.log("false");
+};
+
+// TASK 4 Question...............................................................
+// Write a function that takes 2 numbers as input. If either of the numbers is 3 AND the sum of the numbers contains a 3 then return true. Otherwise return false
+
+// Answer...............................................................................
+
+
+
+
+// TASK 5 Question...............................................................
+// Write a function that takes in three numbers. These numbers represent the lengths of the sides of a triangle. The function should return the area of a triangle.
+
+// Answer...............................................................................
+
+
+
+// TASK 6 Question...............................................................
+// White a function that takes in three numbers and returns the maximum number. Do this without using any builtin methods. Write your own logic from scratch.
+
+// Bonus: How can you change the code so it can take in any number of numbers?
+
+// Answer...............................................................................
+
+
+// TASK 7 Question...............................................................
+// Write a function that takes in a number representing the temperature in Celsius and returns the temperature in Fahrenheit. Write another function that does the opposite (Fereignheit to Celsius)
+
+// Answer...............................................................................
+
+
+
+// TASK 8 Question...............................................................
+// Make a function to convert any number into hours and minutes. (For example, 71 will become “1 hour, 11 minutes”; 133 will become “2 hours, 13 minutes”.)
+
+// Answer...............................................................................
+
+
+// TASK 9 Question...............................................................
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+// Answer...............................................................................
+
+
+// TASK 10 Question...............................................................
+// Write a function that takes in a string and then prints out all the vowels in the string. Make sure it can deal with capital letters and small letters.
+
+// Answer...............................................................................
+
+
+// TASK 11 Question...............................................................
+// Make a function that takes two strings as input, and outputs the common characters/letters that they share. (For example, Input: ‘house’, ‘computers’ . Output: ‘Common letters: o, u, e, s’)
+
+// Answer...............................................................................
+
+
+// TASK 12 Question...............................................................
+// This task isn’t compulsory but we’ll be impressed if you do it.
+
+// Can you make a user interface for each of the previous tasks? You’ll need HTML and CSS to do it.
+
+// Answer...............................................................................
